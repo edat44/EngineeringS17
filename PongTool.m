c98b2f595a5b3c226f0cc9ac4d572c023226a392
edat44 <edat444@gmail.com>
2017-04-08 20:07:51 -0400

    added StartGame function

--- PongTool.m
+++ PongTool.m
@@ -22,7 +22,7 @@ function varargout = PongTool(varargin)
 
 % Edit the above text to modify the response to help PongTool
 
-% Last Modified by GUIDE v2.5 02-Apr-2017 15:56:27
+% Last Modified by GUIDE v2.5 08-Apr-2017 19:59:05
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -46,17 +46,6 @@ end
 
 % --- Executes just before PongTool is made visible.
 function PongTool_OpeningFcn(hObject, eventdata, handles, varargin)
-global startingPosition;
-startingPosition.left = [2, 5];
-startingPosition.right = [10, 5];
-Test();
-% This function has no output args, see OutputFcn.
-% hObject    handle to figure
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-% varargin   command line arguments to PongTool (see VARARGIN)
-
-% Choose default command line output for PongTool
 handles.output = hObject;
 
 % Update handles structure
@@ -77,93 +66,9 @@ function varargout = PongTool_OutputFcn(hObject, eventdata, handles)
 varargout{1} = handles.output;
 
 
-% --- Executes on selection change in dif1popup.
-function dif1popup_Callback(hObject, eventdata, handles)
-% hObject    handle to dif1popup (see GCBO)
+% --- Executes on button press in startButton.
+function startButton_Callback(hObject, eventdata, handles)
+% hObject    handle to startButton (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-
-% Hints: contents = cellstr(get(hObject,'String')) returns dif1popup contents as cell array
-%        contents{get(hObject,'Value')} returns selected item from dif1popup
-
-
-% --- Executes during object creation, after setting all properties.
-function dif1popup_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to dif1popup (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: popupmenu controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
-
-
-% --- Executes on selection change in dif2popup.
-function dif2popup_Callback(hObject, eventdata, handles)
-% hObject    handle to dif2popup (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: contents = cellstr(get(hObject,'String')) returns dif2popup contents as cell array
-%        contents{get(hObject,'Value')} returns selected item from dif2popup
-
-
-% --- Executes during object creation, after setting all properties.
-function dif2popup_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to dif2popup (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: popupmenu controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
-
-
-% --- Executes on selection change in strat1popup.
-function strat1popup_Callback(hObject, eventdata, handles)
-% hObject    handle to strat1popup (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: contents = cellstr(get(hObject,'String')) returns strat1popup contents as cell array
-%        contents{get(hObject,'Value')} returns selected item from strat1popup
-
-
-% --- Executes during object creation, after setting all properties.
-function strat1popup_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to strat1popup (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: popupmenu controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
-
-
-% --- Executes on selection change in strat2popup.
-function strat2popup_Callback(hObject, eventdata, handles)
-% hObject    handle to strat2popup (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: contents = cellstr(get(hObject,'String')) returns strat2popup contents as cell array
-%        contents{get(hObject,'Value')} returns selected item from strat2popup
-
-
-% --- Executes during object creation, after setting all properties.
-function strat2popup_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to strat2popup (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: popupmenu controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
+StartGame(handles);
---
+++ StartGame.m
@@ -0,0 +1,12 @@
+function StartGame(handles)
+
+%% Create both players with correct difficulty and strategy
+
+%% Create ball(s)
+
+%% Start simulation
+while true
+
+end
+
+%% Report game statistics
